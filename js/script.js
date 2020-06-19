39ada6bfa32594772a28bac187aede0135533e65
gergeorg <62167850+gergeorg@users.noreply.github.com>
2020-06-20 00:06:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
 $(document).ready(function () {
 	const mMenuBtn = $('.burger-menu');
 	const mobMenu = $('.m-menu');
+	const tab = $('.tab');
 
 	mMenuBtn.on('click', function() {
-		mobMenu.toggleClass('active-menu')
-		$('body').toggleClass('no-scroll')
+		mobMenu.toggleClass('active-menu');
+		$('body').toggleClass('no-scroll');
+	});
+
+	tab.on('click', function() {
+		tab.removeClass('active');
+		$(this).toggleClass('active');
+
+		let activeTabContent = $(this).attr('data-target');
+		$('.tabs-content').removeClass('visible');
+		$(activeTabContent).toggleClass('visible');
+	});
+
+
+	var mySwiper = new Swiper('.swiper-container', {
+		slidesPerView: 4,
+		spaceBetween: 25,
+		loop: true,
+		breakpoints: {
+			992: {
+				slidesPerView: 4,
+			},
+			768: {
+				slidesPerView: 2,
+			},
+
+			320: {
+				slidesPerView: 1,
+				slidesOffsetAfter: 50,
+				navigation: {
+					nextEl: '.button-next',
+				},
+			},
+		},
 	});
 });
